1bda48435b257367551648d28eb566a4129c6e70
RISHABH-SONII <rishabhsoni@flipr.ai>
2024-11-19 13:23:01 +0530

    fixing global error handler issue

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import createHttpError from "http-errors";
 const app = express();
 
 app.get("/", (req, res, next) => {
-  throw new Error("Something went wrong");
+  const error = createHttpError(400, "Something went wrong");
+  throw error;
   res.json({
     message: "Welcome to elib apis",
   });
