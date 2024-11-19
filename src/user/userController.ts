1cfef66e17fbe22af1b9eb9ac422912faef3f249
RISHABH-SONII <rishabhsoni@flipr.ai>
2024-11-19 16:50:22 +0530

    sign jwt token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,19 +1,61 @@
+// import { NextFunction, Request, Response } from "express";
+// import createHttpError from "http-errors";
+// import userModel from "./userModel";
+// import bcrypt from "bcrypt";
+// import { sign } from "jsonwebtoken";
+// import { config } from "../config/config";
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   // Validation
+
+//   //validation step 1 for required fields
+//   const { name, email, password } = req.body;
+//   if (!name || !email || !password) {
+//     const error = createHttpError(400, "All fields are required");
+//     return next(error); // passing error to globalErrorHandler throgh next(error)
+//   }
+
+//   //validation step 2 to find if user already exists
+//   // Database Call
+//   const user = await userModel.findOne({ email: email });
+
+//   if (user) {
+//     const error = createHttpError(400, "User already exists");
+//     return next(error);
+//   }
+
+//   //password->hashing
+//   //   const hashedPassword = await bcrypt.hash(password, 10);
+//   const hashedPassword = await bcrypt.hash(password, 10);
+//   const newUser = await userModel.create({
+//     name,
+//     email,
+//     password: hashedPassword,
+//   });
+//   // Process
+//   // Token generation JWT
+//   const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+//     expiresIn: "7d",
+//   });
+//   // Response
+//   res.json({ accessToken: token });
+//   res.json({ message: "User Created Successfully" });
+// };
+
+// export { createUser };
+
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  // Validation
-
-  //validation step 1 for required fields
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     const error = createHttpError(400, "All fields are required");
-    return next(error); // passing error to globalErrorHandler throgh next(error)
+    return next(error);
   }
 
-  //validation step 2 to find if user already exists
-  // Database Call
   const user = await userModel.findOne({ email: email });
 
   if (user) {
@@ -21,12 +63,18 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
 
-  //password->hashing
-  //   const hashedPassword = await bcrypt.hash(password, 10);
   const hashedPassword = await bcrypt.hash(password, 10);
-  // Process
-  // Response
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
 
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
+
+  res.json({ accessToken: token });
   res.json({ message: "User Created Successfully" });
 };
 
